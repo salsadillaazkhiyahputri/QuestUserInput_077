48ec8ba02b19a8c72b5bf7930ef5cec641becb5a
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-10-30 12:00:30 +0700

    menambahkan nama lengkap pada form

--- app/src/main/java/com/example/questuserinput/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput/TeksEdit.kt
@@ -1,13 +1,21 @@
 package com.example.questuserinput
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormDataDiri(modifier: Modifier
@@ -24,11 +32,21 @@ fun FormDataDiri(modifier: Modifier
 
     val gender:List<String> = listOf("Laki-laki", "Perempuan")
 
-    Column { modifier.padding(top = 50.dp),
+    Column(modifier=Modifier.padding(top=50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally){
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(250.dp),
+            label = { Text(text = "Nama Lengkap")},
+            onValueChange = {
+                textNama = it
+            }
+        )
     }
-
-
-
 }
+
 
 
