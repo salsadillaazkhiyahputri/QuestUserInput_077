21be0646daa4d1073118e189c33864d78079c879
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-10-30 11:40:11 +0700

    variabel untuk mengingat dari keyboard

---
+++ app/src/main/java/com/example/questuserinput/TeksEdit.kt
@@ -0,0 +1,19 @@
+package com.example.questuserinput
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormDataDiri(modifier: Modifier
+){
+    //variabel-variabel untuk mengingat nilai masukan dari keyboard
+    var textNama by remember {mutableStateOf("")}
+    var textAlamat by remember {mutableStateOf("")}
+    var textJK by remember {mutableStateOf("")}
+}
+
+
