d17f27887f29a291eee5b997866a1bbd38c8f3be
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-10-30 11:42:18 +0700

    membuat variabel alamat untuk menyimpan data

--- app/src/main/java/com/example/questuserinput/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput/TeksEdit.kt
@@ -17,6 +17,7 @@ fun FormDataDiri(modifier: Modifier
 
     //variabel-variabel untuk menyimpan data yang diperoleh dari komponen UI
     var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
 
 }
 
