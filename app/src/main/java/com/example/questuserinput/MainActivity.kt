8ad29163e4cc9df986cf61ed93dc2f1bd8749139
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-10-30 12:42:06 +0700

    memperbaiki main activity yang salah

--- app/src/main/java/com/example/questuserinput/MainActivity.kt
+++ app/src/main/java/com/example/questuserinput/MainActivity.kt
@@ -20,28 +20,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestUserInputTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    FormDataDiri(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestUserInputTheme {
-        Greeting("Android")
     }
 }
