b0b1f811136293be3f496a1e7f3b40ca969e62bb
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-10-30 12:11:14 +0700

    menambahkan divider dengan padding dan warna  pada form

--- app/src/main/java/com/example/questuserinput/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput/TeksEdit.kt
@@ -19,6 +19,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -74,8 +76,13 @@ fun FormDataDiri(modifier: Modifier
             }
         )
         Divider(
-
+            modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium), top = dimensionResource(
+                id = R.dimen.padding_medium
+            )),
+            thickness = dimensionResource(R.dimen.divider_tipis),
+            color = Color.DarkGray
         )
+
     }
 }
 
